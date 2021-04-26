ec2221ce5f1adff5ea502bdc03844ef899010324
DESKTOP-002JUUO\M <mostafa.ad90@gmail.com>
2021-04-27 02:41:53 +0430

    type page animation between state

--- lib/type/type_page.dart
+++ lib/type/type_page.dart
@@ -13,6 +13,7 @@ class TypePage extends StatefulWidget {
 
 class _TypePageState extends State<TypePage> {
   FlutterTts flutterTts = FlutterTts();
+  late WordlistState state;
 
   Future _speak(String w) async {
     var result = await flutterTts.speak(w);
@@ -21,35 +22,88 @@ class _TypePageState extends State<TypePage> {
 
   @override
   Widget build(BuildContext context) {
-    var state = context.watch<WordlistBloc>().state;
+    state = context.watch<WordlistBloc>().state;
     return SizedBox.expand(
-      child: Stack(
-        children: [
+      child: Stack(children: _getStackChildren()
+          // [
           // Background(),
-          SizedBox.expand(
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                ),
-                child: MTypeWriter()),
-          ),
+
           // AppBarOnStack(title: "Type"),
 
-          SizedBox.expand(
-              child: IgnorePointer(
-            ignoring: (state is WordlistStateTypewriter),
-            child: Container(
-                decoration: BoxDecoration(
-                  color: (state is WordlistStateTypewriter)
-                      ? Colors.transparent
-                      : Color.fromRGBO(222, 195, 195, 1),
-                ),
-                child: _getW()),
-          )),
+          // SizedBox.expand(
+          //     child: IgnorePointer(
+          //   ignoring: (state is WordlistStateTypewriter),
+          //   child: Container(
+          //     decoration: BoxDecoration(
+          //       color: (state is WordlistStateTypewriter)
+          //           ? Colors.transparent
+          //           : Color.fromRGBO(222, 195, 195, 1),
+          //     ),
+          //     child: AnimatedSwitcher(
+          //         duration: Duration(seconds: 2), child: _getW()),
+          //   ),
+          // )),
 
-          MWordShow(word: "word", onNextPressed: () {})
-        ],
-      ),
+          // MWordShow(word: "word", onNextPressed: () {})
+          // ],
+          ),
+    );
+  }
+
+  List<Widget> _getStackChildren() {
+    switch (state.runtimeType) {
+      case WordlistStateInitial:
+        return <Widget>[childInitial()];
+      case WordlistStateShow:
+        _speak(state.currentWord);
+        if (state.wordIndex == 0)
+          return <Widget>[childInitial(), childWordShow()];
+        else
+          return <Widget>[childTypeWriter(), childWordShow()];
+      case WordlistStateTypewriter:
+        return <Widget>[childTypeWriter(), childWordShow()];
+    }
+    return <Widget>[childInitial()];
+  }
+
+  Widget childInitial() {
+    return SizedBox.expand(
+        child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(222, 195, 195, 1),
+            ),
+            child: Center(
+              child: IconButton(
+                  iconSize: 120,
+                  icon: Icon(CupertinoIcons.play_fill),
+                  onPressed: () {
+                    Future.delayed(
+                        Duration(seconds: 0),
+                        () => context
+                            .read<WordlistBloc>()
+                            .add(WordlistEventNextClicked()));
+                    // context.read<WordlistBloc>().add(WordlistEventNextClicked());
+                  }),
+            )));
+  }
+
+  Widget childWordShow() {
+    return SizedBox.expand(
+        child: MWordShow(
+      key: Key("lsdkfjlsdfsl"),
+      word: state.currentWord,
+      onNextPressed: () =>
+          context.read<WordlistBloc>().add(WordlistEventNextClicked()),
+    ));
+  }
+
+  Widget childTypeWriter() {
+    return SizedBox.expand(
+      child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+          child: MTypeWriter()),
     );
   }
 
@@ -63,30 +117,40 @@ class _TypePageState extends State<TypePage> {
                 iconSize: 120,
                 icon: Icon(CupertinoIcons.play_fill),
                 onPressed: () {
-                  context.read<WordlistBloc>().add(WordlistEventNextClicked());
+                  Future.delayed(
+                      Duration(seconds: 0),
+                      () => context
+                          .read<WordlistBloc>()
+                          .add(WordlistEventNextClicked()));
+                  // context.read<WordlistBloc>().add(WordlistEventNextClicked());
                 }),
           );
 
         case WordlistStateShow:
           _speak(state.currentWord);
-          return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(state.currentWord,
-                style: Theme.of(context).textTheme.headline4),
-            SizedBox(
-              height: 30,
-            ),
-            Text("${state.wordIndex + 1} of ${state.wordCount}",
-                style: Theme.of(context).textTheme.headline6),
-            SizedBox(
-              height: 60,
-            ),
-            IconButton(
-                iconSize: 120,
-                icon: Icon(CupertinoIcons.arrow_right),
-                onPressed: () {
-                  context.read<WordlistBloc>().add(WordlistEventNextClicked());
-                }),
-          ]);
+          return MWordShow(
+              word: state.currentWord,
+              onNextPressed: () =>
+                  context.read<WordlistBloc>().add(WordlistEventNextClicked()));
+
+        // return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        //   Text(state.currentWord,
+        //       style: Theme.of(context).textTheme.headline4),
+        //   SizedBox(
+        //     height: 30,
+        //   ),
+        //   Text("${state.wordIndex + 1} of ${state.wordCount}",
+        //       style: Theme.of(context).textTheme.headline6),
+        //   SizedBox(
+        //     height: 60,
+        //   ),
+        //   IconButton(
+        //       iconSize: 120,
+        //       icon: Icon(CupertinoIcons.arrow_right),
+        //       onPressed: () {
+        //         context.read<WordlistBloc>().add(WordlistEventNextClicked());
+        //       }),
+        // ]);
 
         case WordlistStateTypewriter:
           _speak(state.currentWord);
--- lib/type/views/m_word_show.dart
+++ lib/type/views/m_word_show.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:typewriter/wordlist/wordlist.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MWordShow extends StatefulWidget {
   final String word;
@@ -21,8 +23,10 @@ class _MWordShowState extends State<MWordShow>
   void initState() {
     super.initState();
 
+    print("_MWordShowState init");
+
     controller =
-        AnimationController(duration: const Duration(seconds: 5), vsync: this);
+        AnimationController(duration: const Duration(seconds: 4), vsync: this);
     // animation = Tween<double>(begin: 0, end: 100).animate(controller)
     //   ..addStatusListener((state) => print('$state'));
     animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
@@ -35,7 +39,8 @@ class _MWordShowState extends State<MWordShow>
         }
       });
 
-    controller.forward();
+    // controller.forward();
+    //
   }
 
   @override
@@ -46,87 +51,186 @@ class _MWordShowState extends State<MWordShow>
 
   @override
   Widget build(BuildContext context) {
+    var state = context.watch<WordlistBloc>().state;
+    if (state is WordlistStateShow) controller.forward();
+
     return Padding(
       padding: EdgeInsets.all(0),
       child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(6),
-        ),
-        child: AnimatedLogo(
-            animation: animation,
+        // decoration: BoxDecoration(
+        //   color: Colors.white,
+        //   borderRadius: BorderRadius.circular(6),
+        // ),
+        child: StaggerAnimation(
+            controller: controller,
             word: widget.word,
             onNextPressed: () {
               controller.reverse();
               widget.onNextPressed();
+              // Future.delayed(
+              //     Duration(seconds: 0, milliseconds: 500), () => widget.onNextPressed());
+              // widget.onNextPressed();
             },
             height: MediaQuery.of(context).size.height),
+
+        // AnimatedLogo(
+        //     animation: animation,
+        //     word: widget.word,
+        //     onNextPressed: () {
+        //       controller.reverse();
+        //       widget.onNextPressed();
+        //       // Future.delayed(
+        //       //     Duration(seconds: 0, milliseconds: 500), () => widget.onNextPressed());
+        //       // widget.onNextPressed();
+        //     },
+        //     height: MediaQuery.of(context).size.height),
       ),
     );
   }
 }
 
-class AnimatedLogo extends AnimatedWidget {
-  final String word;
-  final VoidCallback onNextPressed;
-  final double height;
-  // Make the Tweens static because they don't change.
-  // static final _opacityTween =
-  //     Tween<double>(begin: height * -1, end: 0);
-  static final _sizeTween = Tween<double>(begin: 0, end: 300);
-
-  AnimatedLogo(
+class StaggerAnimation extends StatelessWidget {
+  StaggerAnimation(
       {Key? key,
-      required Animation<double> animation,
+      required this.controller,
       required this.word,
       required this.onNextPressed,
       required this.height})
-      : super(key: key, listenable: animation);
+      : backgroundYPosition = Tween<double>(begin: -height, end: 0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.0,
+              0.300,
+              curve: Curves.easeInCubic,
+            ),
+          ),
+        ),
+        scale1Tween = Tween<double>(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.200,
+              0.600,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        buttonYPositionTween = Tween<double>(
+          begin: height,
+          end: 0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.6,
+              1.0,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        padding = EdgeInsetsTween(
+          begin: const EdgeInsets.only(bottom: 16.0),
+          end: const EdgeInsets.only(bottom: 75.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        borderRadius = BorderRadiusTween(
+          begin: BorderRadius.circular(4.0),
+          end: BorderRadius.circular(75.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.375,
+              0.500,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        color = ColorTween(
+          begin: Colors.red,
+          end: Colors.blue,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.500,
+              0.750,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
 
-  Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    var _downTween = Tween<double>(begin: height * -1, end: height);
-    return Center(
-      child: Transform.translate(
-        offset: Offset(
-            0,
-            _downTween.evaluate(animation) > 0
-                ? 0
-                : _downTween.evaluate(animation)),
-        child: SizedBox.expand(
+        // ... Other tween definitions ...
+
+        super(key: key);
+
+  final AnimationController controller;
+  final String word;
+  final VoidCallback onNextPressed;
+  final double height;
+  final Animation<double> scale1Tween;
+  final Animation<double> buttonYPositionTween;
+  final Animation<double> backgroundYPosition;
+  final Animation<EdgeInsets> padding;
+  final Animation<BorderRadius> borderRadius;
+  final Animation<Color?> color;
+
+  // This function is called each time the controller "ticks" a new frame.
+  // When it runs, all of the animation's values will have been
+  // updated to reflect the controller's current value.
+  Widget _buildAnimation(BuildContext context, Widget? child) {
+    return Stack(children: [
+      SizedBox.expand(
+        child: Transform.translate(
+          offset: Offset(0, backgroundYPosition.value),
           child: Container(
             decoration: BoxDecoration(
-              color: Color.fromRGBO(222, 195, 195, 1),
+              color: Color.fromRGBO(222, 80, 195, 1),
             ),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Transform.scale(
-                scale: _downTween.evaluate(animation) < 0
-                    ? 0
-                    : _downTween.evaluate(animation) / height,
-                child: Text(word, style: Theme.of(context).textTheme.headline4),
-              ),
-              SizedBox(
-                height: 100, // _sizeTween.evaluate(animation),
-              ),
-              Transform.translate(
-                offset: Offset(
-                  0,
-                  _downTween.evaluate(animation) < 0
-                      ? _downTween.evaluate(animation) * -1 - height
-                      : (height - _downTween.evaluate(animation)),
-                ),
-                child: IconButton(
-                    iconSize: 64, //_sizeTween.evaluate(animation),
-                    icon: Icon(CupertinoIcons.arrow_right),
-                    onPressed: () {
-                      onNextPressed();
-                    }),
-              )
-            ]),
           ),
         ),
       ),
+      SizedBox.expand(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Transform.scale(
+          scale: scale1Tween.value,
+          child: Text(word, style: Theme.of(context).textTheme.headline4),
+        ),
+        SizedBox(
+          height: 100, // _sizeTween.evaluate(animation),
+        ),
+        Transform.scale(
+            scale: 1, //scale1Tween.value,
+            child: Transform.translate(
+              offset: Offset(0, buttonYPositionTween.value),
+              child: IconButton(
+                  iconSize: 64, //_sizeTween.evaluate(animation),
+                  icon: Icon(CupertinoIcons.arrow_right),
+                  onPressed: () {
+                    onNextPressed();
+                  }),
+            ))
+      ])),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: controller,
     );
   }
 }
