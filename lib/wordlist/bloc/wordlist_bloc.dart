0c04f4fa1cdf46e022fb7cf997bb19ee21d3a6c7
emoo <emoo44566@gmail.com>
2022-08-21 16:43:48 +0430

    upgrade bloc to 8.1.1

--- lib/wordlist/bloc/wordlist_bloc.dart
+++ lib/wordlist/bloc/wordlist_bloc.dart
@@ -13,7 +13,45 @@ class WordlistBloc extends Bloc<WordlistEvent, WordlistState> {
 
   WordlistBloc({required this.repository})
       : super(WordlistStateInitial(
-            packageModel: PackageModel(0, "title", "description", 10)));
+            packageModel: PackageModel(0, "title", "description", 10))) {
+    on<WordlistEventStartClicked>((event, emit) async => {
+          if (state is WordlistStateInitial)
+            {
+              emit(WordlistStateShow(
+                  packageModel: event.packageModel,
+                  currentWord:
+                      await repository.getWord(event.packageModel.id, 0),
+                  wordIndex: 0))
+            }
+        });
+
+    on<WordlistEventNextClicked>((event, emit) async => {
+          if (state is WordlistStateInitial)
+            emit(WordlistStateShow(
+                packageModel: state.packageModel,
+                currentWord: await repository.getWord(state.packageModel.id, 0),
+                wordIndex: 0))
+          else if (state is WordlistStateShow)
+            emit(WordlistStateTypewriter(
+                packageModel: state.packageModel,
+                currentWord: state.currentWord,
+                wordIndex: state.wordIndex))
+          else if (state is WordlistStateTypewriter)
+            if (state.wordIndex + 1 == state.packageModel.wordCount)
+              emit(WordlistStateFinish(
+                  packageModel: state.packageModel,
+                  currentWord: const MapEntry<String, String>('', ''),
+                  wordIndex: state.wordIndex))
+            else
+              emit(WordlistStateShow(
+                  packageModel: state.packageModel,
+                  currentWord: await repository.getWord(
+                      state.packageModel.id, state.wordIndex + 1),
+                  wordIndex: state.wordIndex + 1))
+          else if (state is WordlistStateFinish)
+            emit(WordlistStateInitial(packageModel: state.packageModel))
+        });
+  }
 
   @override
   void onTransition(Transition<WordlistEvent, WordlistState> transition) {
@@ -21,56 +59,4 @@ class WordlistBloc extends Bloc<WordlistEvent, WordlistState> {
     super.onTransition(transition);
   }
 
-  @override
-  Stream<WordlistState> mapEventToState(
-    WordlistEvent event,
-  ) async* {
-    if (event is WordlistEventStartClicked) {
-      if (state is WordlistStateInitial) {
-        yield WordlistStateShow(
-            packageModel: event.packageModel,
-            currentWord: await repository.getWord(event.packageModel.id, 0),
-            wordIndex: 0);
-      }
-    }
-    if (event is WordlistEventNextClicked) {
-      switch (state.runtimeType) {
-        case WordlistStateInitial:
-          yield WordlistStateShow(
-              packageModel: state.packageModel,
-              currentWord: await repository.getWord(state.packageModel.id, 0),
-              wordIndex: 0);
-          break;
-        case WordlistStateShow:
-          yield WordlistStateTypewriter(
-              packageModel: state.packageModel,
-              currentWord: state.currentWord,
-              wordIndex: state.wordIndex);
-          break;
-        case WordlistStateTypewriter:
-          if (state.wordIndex + 1 == state.packageModel.wordCount)
-            yield WordlistStateFinish(
-                packageModel: state.packageModel,
-                currentWord: const MapEntry<String, String>('', ''),
-                wordIndex: state.wordIndex);
-          else
-            yield WordlistStateShow(
-                packageModel: state.packageModel,
-                currentWord: await repository.getWord(
-                    state.packageModel.id, state.wordIndex + 1),
-                wordIndex: state.wordIndex + 1);
-          break;
-        case WordlistStateFinish:
-          yield WordlistStateInitial(packageModel: state.packageModel);
-          break;
-      }
-
-      // yield* _mapWordlistSelectTClickedsToState(event);
-    }
-  }
-
-  // Stream<WordlistState> _mapWordlistSelectTClickedsToState(
-  //     WordlistEventNextWordShowed event) async* {
-  //   // yield WordlistStateNext(selectedT: event.seletedT);
-  // }
 }
